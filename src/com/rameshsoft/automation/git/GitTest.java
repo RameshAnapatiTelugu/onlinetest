4efec7d779ce7fb515b01144f5d5e538b1d9f459
RameshAnapatiTelugu <rameshsoftonline@gmail.com>
2018-07-07 22:28:43 +0300

    added syso by 2nd user

--- src/com/rameshsoft/automation/git/GitTest.java
+++ src/com/rameshsoft/automation/git/GitTest.java
@@ -3,5 +3,6 @@ package com.rameshsoft.automation.git;
 public class GitTest {
 public static void main(String[] args) {
 	System.out.println("Hello WelCome To RAMESHSOFT");
+	System.out.println("Hello...JAVA");
 }
 }
